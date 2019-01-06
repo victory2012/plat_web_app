168978e22eedcbffc2cd0f21eb194f84f4011df6
victory2012 <mthzsl@126.com>
2019-01-06 18:59:18 +0800

    update

--- src/login/login.vue
+++ src/login/login.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="loginPage">
     <transition name='slide-fade'>
-      <div class="card ad" v-show="showAD">this is 开屏广告</div>
+      <div class="card ad" v-show="showAD">
+        <div class="adinfo">
+          摩融电池检测管理平台
+        </div>
+      </div>
     </transition>
     <transition name='slide-fade'>
       <div class="card loginCenter" v-show="!showAD">
-        <cube-button @click="signIn">Light Button</cube-button>
+        <cube-button @click="signIn">test login</cube-button>
       </div>
     </transition>
   </div>
@@ -22,7 +26,7 @@ export default {
   mounted () {
     setTimeout(() => {
       this.showAD = false
-    }, 2000)
+    }, 3000)
   },
   methods: {
     signIn () {
@@ -39,6 +43,13 @@ export default {
   height 100%
   .card
     height 100%
+    .adinfo
+      width 100%
+      height 40vh
+      text-align center
+      line-height 40vh
+      color #ffffff
+      font-size 18px
     &.ad
       background-color blue
     &.loginCenter
